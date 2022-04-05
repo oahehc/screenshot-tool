c84799f5e44ae44fd0c9f41add2c432014fab712
oahehc(Andrew) <oahehc@gmail.com>
2022-04-05 18:45:28 +0900

    feat: add device options and optimize logs

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
+import fs from'fs';
 import puppeteer from 'puppeteer';
 import ora from 'ora';
 import inquirer from 'inquirer';
 
 // https://github.com/puppeteer/puppeteer/blob/main/src/common/DeviceDescriptors.ts
-const devices = ['iPhone SE', 'iPhone XR', 'iPad Pro'];
-const desktopSizes = [
-  { w: 1600, h: 1200 },
-  { w: 1280, h: 800 },
-  { w: 980, h: 600 },
-];
+// const devices = ['iPhone SE', 'iPhone XR', 'iPad Pro'];
+const deviceMap = {
+  iPhone4: { w: 320, h: 480 },
+  iPhoneSE: { w: 375, h: 667 },
+  iPhoneXR: { w: 414, h: 896 },
+  iPadPro: { w: 1024, h: 1366 },
+  Laptop: { w: 1440 , h: 800 },
+  DT: { w: 2560 , h: 1600 },
+};
+const dir = './screenshots';
+
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir);
+}
 
 (function screenshot() {
   inquirer
@@ -25,10 +34,11 @@ const desktopSizes = [
         },
       },
       {
-        type: 'list',
-        name: 'device',
-        message: 'select the device',
-        choices: ['all', 'desktop', 'mobile'],
+        type: 'checkbox',
+        name: 'devices',
+        default: ['iPhone4', 'iPhoneXR', 'iPadPro', 'DT'],
+        message: 'select the devices',
+        choices: Object.keys(deviceMap).map((val) => ({ name: val, value: val })),
       },
       {
         type: 'number',
@@ -37,60 +47,39 @@ const desktopSizes = [
         default: 1000,
       },
     ])
-    .then(({ targetUrl, device, waitFor }) => {
-      const spinner = ora('take screenshot: ').start();
+    .then(({ targetUrl, devices, waitFor }) => {
       (async () => {
         try {
+          const spinner = ora(`loading`).start();
           const browser = await puppeteer.launch({ headless: true });
           // const browser = await puppeteer.launch({ headless: false, slowMo: 2000 });
           const page = await browser.newPage();
           await page.goto(targetUrl, { waitUntil: 'networkidle2' });
-          if (device === 'mobile' || device === 'all') {
-            for (let device of devices) {
-              await page.emulate(puppeteer.devices[device]);
+          const sizes = devices.map((d) => deviceMap[d]);
+          spinner.stop();
+          for (let size of sizes) {
+            const { w, h } = size;
+            const spinner = ora(`W${w}xH${h}`).start();
+            await page.setViewport({ width: w, height: h });
 
-              await page.emulateMediaFeatures([{ name: 'prefers-color-scheme', value: 'light' }]);
-              await page.waitForTimeout(waitFor);
-              await page.screenshot({
-                path: `./screenshots/${Date.now()}_${device.replace(/\s/, '_')}.jpg`,
-                fullPage: true,
-                quality: 60,
-              });
-              await page.emulateMediaFeatures([{ name: 'prefers-color-scheme', value: 'dark' }]);
-              await page.waitForTimeout(waitFor);
-              await page.screenshot({
-                path: `./screenshots/${Date.now()}_${device.replace(/\s/, '_')}_dark.jpg`,
-                fullPage: true,
-                quality: 60,
-              });
-            }
+            await page.emulateMediaFeatures([{ name: 'prefers-color-scheme', value: 'light' }]);
+            await page.waitForTimeout(waitFor);
+            await page.screenshot({
+              path: `${dir}/${Date.now()}_W${w}xH${h}.jpg`,
+              fullPage: true,
+              quality: 60,
+            });
+            await page.emulateMediaFeatures([{ name: 'prefers-color-scheme', value: 'dark' }]);
+            await page.waitForTimeout(waitFor);
+            await page.screenshot({
+              path: `${dir}/${Date.now()}_W${w}xH${h}_dark.jpg`,
+              fullPage: true,
+              quality: 60,
+            });
+            spinner.succeed(`W${w}xH${h}`);
           }
-
-          if (device === 'desktop' || device === 'all') {
-            for (let size of desktopSizes) {
-              const { w, h } = size;
-              await page.setViewport({ width: w, height: h });
-
-              await page.emulateMediaFeatures([{ name: 'prefers-color-scheme', value: 'light' }]);
-              await page.waitForTimeout(waitFor);
-              await page.screenshot({
-                path: `./screenshots/${Date.now()}_W${w}xH${h}.jpg`,
-                fullPage: true,
-                quality: 60,
-              });
-              await page.emulateMediaFeatures([{ name: 'prefers-color-scheme', value: 'dark' }]);
-              await page.waitForTimeout(waitFor);
-              await page.screenshot({
-                path: `./screenshots/${Date.now()}_W${w}xH${h}_dark.jpg`,
-                fullPage: true,
-                quality: 60,
-              });
-            }
-          }
-          spinner.succeed('finish');
           await browser.close();
         } catch (e) {
-          spinner.fail('error');
           console.log('error:', e);
         }
       })();
